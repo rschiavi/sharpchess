04742a02af7ebf44e053673c2b487f2d9a19df3b
rschiavi <mrc_rafa@hotmail.com>
2020-03-28 17:51:32 -0300

    possible movements of the king

--- sharpchess/board/Piece.cs
+++ sharpchess/board/Piece.cs
@@ -1,6 +1,6 @@
 ﻿namespace board
 {
-    class Piece
+    abstract class Piece
     {
         public Position Position { get; set; }
         public Color Color { get; protected set; }
@@ -19,5 +19,7 @@
         {
             QtyMovements++;
         }
+
+        public abstract bool[,] PossibleMovements();
     }
 }
--- sharpchess/chess/King.cs
+++ sharpchess/chess/King.cs
@@ -12,5 +12,84 @@ namespace chess
         {
             return "K";
         }
+
+        private bool canMove(Position pos)
+        {
+            Piece p = Board.GetPiece(pos);
+            return p == null || p.Color != Color;
+        }
+
+        public override bool[,] PossibleMovements()
+        {
+            bool[,] matrix = new bool[Board.Rows, Board.Cols];
+
+            Position pos = new Position(0, 0);
+
+            // cima
+            pos.Row = Position.Row -1;
+            pos.Col = Position.Col;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // cima direita
+            pos.Row = Position.Row -1;
+            pos.Col = Position.Col + 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // direita
+            pos.Row = Position.Row;
+            pos.Col = Position.Col + 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // baixo direita
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col + 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // baixo
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // baixo esquerda
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col - 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // esquerda
+            pos.Row = Position.Row;
+            pos.Col = Position.Col - 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            // cima esquerda
+            pos.Row = Position.Row - 1;
+            pos.Col = Position.Col - 1;
+            if (Board.IsValidPosition(pos) && canMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+            }
+
+            return matrix;
+        }
     }
 }
