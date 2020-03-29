60764cbe44aa32e42faa1b38582cb9cdf528529d
rschiavi <mrc_rafa@hotmail.com>
2020-03-29 02:02:51 -0300

    Queen movements implemented

--- sharpchess/chess/Queen.cs
+++ sharpchess/chess/Queen.cs
@@ -19,7 +19,113 @@ namespace chess
 
             Position pos = new Position(0, 0);
 
+            // up
+            pos.Row = Position.Row - 1;
+            pos.Col = Position.Col;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row--;
+            }
 
+            // up right
+            pos.Row = Position.Row - 1;
+            pos.Col = Position.Col + 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row--;
+                pos.Col++;
+            }
+
+            // right
+            pos.Row = Position.Row;
+            pos.Col = Position.Col + 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Col++;
+            }
+
+            // down right
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col + 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row++;
+                pos.Col++;
+            }
+
+            // down
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row++;
+            }
+
+            // down left
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col - 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row++;
+                pos.Col--;
+            }
+
+            // left
+            pos.Row = Position.Row;
+            pos.Col = Position.Col - 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Col--;
+            }
+
+            // up left
+            pos.Row = Position.Row - 1;
+            pos.Col = Position.Col - 1;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row--;
+                pos.Col--;
+            }
 
             return matrix;
         }
--- sharpchess/chess/Rook.cs
+++ sharpchess/chess/Rook.cs
@@ -32,19 +32,6 @@ namespace chess
                 pos.Row--;
             }
 
-            // down
-            pos.Row = Position.Row + 1;
-            pos.Col = Position.Col;
-            while (Board.IsValidPosition(pos) && CanMove(pos))
-            {
-                matrix[pos.Row, pos.Col] = true;
-                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
-                {
-                    break;
-                }
-                pos.Row++;
-            }
-
             // right
             pos.Row = Position.Row;
             pos.Col = Position.Col + 1;
@@ -56,6 +43,19 @@ namespace chess
                     break;
                 }
                 pos.Col++;
+            }
+
+            // down
+            pos.Row = Position.Row + 1;
+            pos.Col = Position.Col;
+            while (Board.IsValidPosition(pos) && CanMove(pos))
+            {
+                matrix[pos.Row, pos.Col] = true;
+                if (Board.GetPiece(pos) != null && Board.GetPiece(pos).Color != Color)
+                {
+                    break;
+                }
+                pos.Row++;
             }
 
             // left
