1052fef38943ea0717d2e163c915052cc01166d7
rschiavi <mrc_rafa@hotmail.com>
2020-03-29 02:10:07 -0300

    Pawn movements implemented

--- sharpchess/chess/Pawn.cs
+++ sharpchess/chess/Pawn.cs
@@ -13,12 +13,83 @@ namespace chess
             return "P";
         }
 
+        private bool HasOpponent(Position pos)
+        {
+            Piece piece = Board.GetPiece(pos);
+            return piece != null && piece.Color != Color;
+        }
+
+        private bool FreePosition(Position pos)
+        {
+            return Board.GetPiece(pos) == null;
+        }
+
         public override bool[,] PossibleMovements()
         {
             bool[,] matrix = new bool[Board.Rows, Board.Cols];
 
             Position pos = new Position(0, 0);
 
+            if (Color == Color.White)
+            {
+                pos.Row = Position.Row - 1;
+                pos.Col = Position.Col;
+                if (Board.IsValidPosition(pos) && FreePosition(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row - 2;
+                pos.Col = Position.Col;
+                if (Board.IsValidPosition(pos) && FreePosition(pos) && QtyMovements == 0)
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row - 1;
+                pos.Col = Position.Col - 1;
+                if (Board.IsValidPosition(pos) && HasOpponent(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row - 1;
+                pos.Col = Position.Col + 1;
+                if (Board.IsValidPosition(pos) && HasOpponent(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+            }
+            else
+            {
+                pos.Row = Position.Row + 1;
+                pos.Col = Position.Col;
+                if (Board.IsValidPosition(pos) && FreePosition(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row + 2;
+                pos.Col = Position.Col;
+                if (Board.IsValidPosition(pos) && FreePosition(pos) && QtyMovements == 0)
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row + 1;
+                pos.Col = Position.Col - 1;
+                if (Board.IsValidPosition(pos) && HasOpponent(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+
+                pos.Row = Position.Row + 1;
+                pos.Col = Position.Col + 1;
+                if (Board.IsValidPosition(pos) && HasOpponent(pos))
+                {
+                    matrix[pos.Row, pos.Col] = true;
+                }
+            }
 
 
             return matrix;
